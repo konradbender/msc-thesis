600e3f5557875e1636df1efad77c5a9f39604a64
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-07-10 11:01:38 +0100

    add Numba

--- python/glauber2D.py
+++ python/glauber2D.py
@@ -6,117 +6,111 @@ import matplotlib.pyplot as plt
 import json
 import datetime
 import os
+from numba import jit, njit, prange
+from numba.typed import Dict
+from numba.core import types
 
-def squary_boundary_fix(n, p, boundary=1):
-    """Returns a 2-dimensional matrix of size n, vertices initialized 
-    to 1 or 0 with probability p"""
-    # gives it out with a buffer, so a 2x2x2 cube would be 3x3x3
+LOGGING_STEP = 1000
+BOUNDARY = 1
 
-    shape = [n]*2
-    matrix =  np.random.binomial(n=1, p=p, size=shape)
-    matrix[0,:] = boundary
-    matrix[-1,:] = boundary
-
-    return matrix
-
-def squary_boundary_random(n, p, boundary=1):
-    """Returns a 2-dimensional matrix of size n, vertices initialized 
-    to 1 or 0 with probability p"""
-    # gives it out with a buffer, so a 2x2x2 cube would be 3x3x3
-
-    shape = [n]*2
-    matrix =  np.random.binomial(n=1, p=p, size=shape)
-
-    return matrix
-
-def get_random_indices(n, t):
-    """Returns t random indices for a 2-dimensional lattice of size n, but will not choose 
-    one from the boundary"""
-
-    # for randint, the first argument is inclusive, the second is exclusive
-    return np.random.randint(1, n-1, size=2*t).reshape((t, 2))
-    
-
-def run_single_glauber(n_outer, n_interior, p, t, tol):
+@njit(cache = True, parallel = False)
+def run_single_glauber(
+    n_outer: np.int64, n_interior: np.int64, p: np.float64, t: np.int64, tol: np.float64, run_id: int = None,
+    verbose: bool = False
+) -> dict:
     """Runs a simulation of the Glauber dynamics on a d-dimensional lattice of size n
     with probability p of initializing a vertex to 1"""
 
     t = int(t)
 
-    matrix = squary_boundary_fix(n_outer,p)
-    indices = get_random_indices(n_outer,t)
-    
-    buffer = (n_outer - n_interior) // 2
-    interior_indices = tuple([slice(buffer,
-                                    -buffer)]*2)
+    matrix = np.random.binomial(n=1, p=p, size=n_outer**2).astype(np.bool_)
+    matrix = matrix.reshape((n_outer, n_outer))
+    matrix[0, :] = BOUNDARY
+    matrix[-1, :] = BOUNDARY
+
+    indices = np.random.randint(1, n_outer - 1, size=2 * t).reshape((t, 2))
+
+    buffer = (n_outer - n_interior) // np.int64(2)
+    interior_indices = (slice(buffer, -buffer), slice(buffer, -buffer))
+
+    target = matrix[interior_indices].shape[0] * matrix[interior_indices].shape[1]
+
+    vector = np.ones(t) * np.int64(-1)
 
     iterations = 0
-    fixation = False
-    for index in indices:
+    fixation = np.bool_(False)
+    for i, index in enumerate(indices):
         iterations += 1
         """Updates the vertex at index in the matrix"""
 
-        neighbor_indices = np.array([index + np.array([0,1]),
-                            index + np.array([1,0]),
-                            index + np.array([-1,0]),
-                            index + np.array([0,-1]),
-                            ])
-        
-        neighbor_indices = tuple(neighbor_indices.T.reshape((2, 4)))
+        nb_sum = (
+            np.int64(matrix[index[0] + 1, index[1]])
+            + np.int64(matrix[index[0], index[1] + 1])
+            + np.int64(matrix[index[0] - 1, index[1]])
+            + np.int64(matrix[index[0], index[1] - 1])
+        )
 
-
-
-        nb_sum = np.sum(matrix[neighbor_indices])
-        
         # d is half the number of neighbors
-        if nb_sum > 2:
+        if nb_sum > np.int64(2):
             # again add one to index because of buffer
-            matrix[tuple(index)] = 1
-            
-        # d is half the number of neighbors
-        if nb_sum < 2:
-            # again add one to index because of buffer
-            matrix[tuple(index)] = 0
+            matrix[index[0], index[1]] = 1
 
-        if nb_sum == 2:
+        # d is half the number of neighbors
+        if nb_sum < np.int64(2):
+            # again add one to index because of buffer
+            matrix[index[0], index[1]] = 0
+
+        if nb_sum == np.int64(2):
             # flip coin
-            z = np.random.binomial(n=1, p=0.5)
-            matrix[tuple(index)] = z
-            
-            
-        if sum(matrix[interior_indices].flatten()) >= \
-            tol*matrix[interior_indices].shape[0]*matrix[interior_indices].shape[1]:
-            fixation = True
+            z = np.random.binomial(n=1, p=np.float64(0.5))
+            matrix[index[0], index[1]] = z
+
+        summed_array = np.sum(matrix[interior_indices].flatten())
+
+        vector[i] = summed_array / target
+
+        if summed_array >= tol * target:
+            fixation = np.bool_(True)
             break
-        elif sum(matrix[interior_indices].flatten()) <= \
-            (1-tol) * matrix[interior_indices].shape[0]*matrix[interior_indices].shape[1]:
-            fixation = False
+        elif summed_array <= (1 - tol) * target:
+            fixation = np.bool_(False)
             break
 
-        if iterations % 1000 == 0:
-            print(f"iteration {iterations} of {t} for probability {p}")
+        if (iterations % LOGGING_STEP == 0) and verbose:
+            print("iteration:", iterations, "share of 1 is:", (summed_array / target))
     
-    result = {"fixation": fixation, "iterations": iterations,
-            "p": p, "n_outer": n_outer, "n_interior": n_interior, "t": t, "tol": tol, 
-            }
-    print("finished a single run of glauber dynamics with result: ", result)
-    
+    result = Dict.empty(
+        key_type=types.unicode_type,
+        value_type=types.float64[:]
+    )
+    result.update({"fixation": np.asarray([np.float64(fixation)]),
+                    "iterations": np.asarray([np.float64(iterations)]), 
+                    "vector": vector})
+
     return result
 
-def run_simulation(n_outer, n_inner, p, t, iter, tol):
-
+@njit(cache = True, parallel = False)
+def run_fixation_simulation(
+    n_outer: int, n_inner: int, p: float, t: int, iter: int, tol: float,
+    verbose: bool = False
+) -> dict:
     fixations = 0
     iterations = 0
+    results = []
 
-    for i in range(iter):
-        result = run_single_glauber(n_outer, n_inner, p, t, tol)
-
+    for i in prange(iter):
+        result = run_single_glauber(n_outer, n_inner, p, t, tol, verbose=verbose)
+        results.append(result)
+        fixations += result["fixation"]
         if result["fixation"]:
-            fixations += 1
             iterations += result["iterations"]
 
-
-    return {'fixation_rate': fixations / iter,
-            'mean_iterations': iterations / iter,
-            "p": p, "n_outer": n_outer,"n_inner": n_inner, "t": t}
-
+    mean_iterations = iterations / fixations if fixations > 0 else 0
+    return {
+        "fixation_rate": fixations / iter,
+        "mean_iterations_when_fix": mean_iterations,
+        "p": p,
+        "n_outer": n_outer,
+        "n_inner": n_inner,
+        "t": t,
+    }
