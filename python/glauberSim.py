a2f0f233a6d423f1208eb7065d37ec256b4fda08
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-08-13 14:11:00 +0100

    change padding parameter logic

--- python/glauberSim.py
+++ python/glauberSim.py
@@ -5,20 +5,21 @@ class GlauberSim(ABC):
 
     def __init__(
         self,
-        n_outer: np.int64,
         n_interior: np.int64,
         p: np.float64,
         t: np.int64,
         tol: np.float64,
+        padding : np.int64 = None, # for new code
+        n_outer: np.int64 = None # kept for depreceated compatibility
     ) -> None:
         """Runs a simulation of the Glauber dynamics on a d-dimensional lattice of size n
         with probability p of initializing a vertex to 1
 
         Parameters
         ----------
-        n_outer : int
-            dimension of outer lattice
-        n_inner : int
+        padding : int
+            number of vertices beyond the border of n_interior*n_interior matrix
+        n_interior : int
             dimension of inner lattice
         p : float
             probability that vertices are -1 at t=0
@@ -27,11 +28,25 @@ class GlauberSim(ABC):
         tol : float
             minimum share of -1 vertices to reach to declare fixation
         """
-        self.n_outer = n_outer
+        if padding is None and n_outer is None:
+            raise ValueError("padding or n_outer must be specified")    
+        
+        if padding is not None:
+            self.padding = padding
+            self.n_outer = n_interior + 2 * padding
+
+        if n_outer is not None:
+            raise DeprecationWarning("n_outer is deprecated, use padding instead")
+            self.n_outer = self.n_outer
+            self.padding = (self.n_outer - n_interior) // 2
+
+
         self.n_interior = n_interior
         self.p = p
         self.t = t
         self.tol = tol
+
+        
 
     @abstractmethod
     def run_single_glauber( *args, **kwargs) -> dict:
