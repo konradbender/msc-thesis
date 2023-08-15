62935908b73a460bd847f07a5b80fa3a211678ad
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-08-15 18:57:28 +0100

    fix email

--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --job testing
 #SBATCH --cpus-per-task 5
 #SBATCH --mem 16G
-#SBATCH --mail-user=kbender@exeter.ox.ac.uk
+#SBATCH --mail-user=konrad.bender@exeter.ox.ac.uk
 #SBATCH --partition=standard-cpu
 
 echo “I am job number ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} running on the computer ${HOSTNAME}”
