8b6ae65af34e43f25eb75921d184c2247ccdcb12
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-08-15 17:57:57 +0100

    add submit file

---
+++ python/submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job testing
+#SBATCH --cpus-per-task 5
+#SBATCH --mem 5G
+#SBATCH --mail-user=kobender@stats.ox.ac.uk
+#SBATCH --partition=standard-cpu
+
+echo “I am job number ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} running on the computer ${HOSTNAME}”
+
+# activate venv
+python3 test_glauber.py
