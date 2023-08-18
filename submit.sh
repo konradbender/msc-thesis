d31d0490298985e84fa3ac5615d372f49ef8cebe
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-08-18 15:43:47 +0100

    submit script

--- submit.sh
+++ submit.sh
@@ -7,17 +7,10 @@
 N=4
 #SBATCH --cpus-per-task 5
 
-#SBATCH --mail-user=kobender@stats.ox.ac.uk
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-type=REQUEUE
-#SBATCH --mail-type=ALL
-
 echo “I am job number ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} running on the computer ${HOSTNAME}”
 
 # activate venv
-source thesis-env-2/bin/activate
+source /vols/teaching/msc-projects/2022-2023/kobender/msc-thesis/thesis-env-2/bin/activate
 
 T=20000000 # time steps
 N_INT=500 # n_interior, so the size of the grid
