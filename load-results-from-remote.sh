ffa8cb771bec8799b3a7e93128f4898372da8f3d
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-08-18 15:21:49 +0100

    adapt shell scripts

---
+++ load-results-from-remote.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FILE_PATH="/vols/teaching/msc-projects/2022-2023/kobender/msc-thesis/results/0817_11-39-45/"
+
+scp -r stats-swan:$FILE_PATH ./results/from-remote/
--- submit.sh
+++ submit.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-#SBATCH --job testing
+#SBATCH --job redo-the-500-20M
 #SBATCH --mem 16G
-#SBATCH --mail-user=kobender@stats.ox.ac.uk
 #SBATCH --partition=standard-cpu
+
+# THE FOLLOWING TWO MUST BE MANUALLY ALIGNED -  ONCE ORE MORE THAN ITERS
+N_INT=4
+#SBATCH --cpus-per-task 5
+
+#SBATCH --mail-user=kobender@stats.ox.ac.uk
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
 
 echo “I am job number ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} running on the computer ${HOSTNAME}”
 
 # activate venv
 source thesis-env-2/bin/activate
 
-T=2000
+T=20000000
 N=4
-CHECKPOINT=10000
-
-# THE FOLLOWING TWO MUST BE MANUALLY ALIGNED
-N_INT=4
-#SBATCH --cpus-per-task 5
+CHECKPOINT=1000000
 
 PADDING=10
 
