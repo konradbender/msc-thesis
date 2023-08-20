37926ba2ab7cf17177fff1cd19c7e7cb1c3f4270
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-08-20 12:33:51 +0100

    make temp dirs

--- python/test_glauber.py
+++ python/test_glauber.py
@@ -14,8 +14,10 @@ import shutil
 @pytest.mark.parametrize("class_to_test", (GlauberSimulatorFixIndices,))
 class TestSingleGlauber:
 
-    def test_small_1(self, class_to_test: type[GlauberSim]):
-        sim = class_to_test(padding=1, n_interior=5, p=0.7, t=200, tol=0.98, random_seed=0)
+    def test_small_1(self, class_to_test: type[GlauberSim], tmpdir):
+        tmpdir = str(tmpdir)
+        sim = class_to_test(padding=1, n_interior=5, p=0.7, t=200, tol=0.98, random_seed=0,
+                            results_dir=tmpdir)
         result = sim.run_single_glauber(True)
         np.testing.assert_array_equal(
             result["vector"],
@@ -225,13 +227,15 @@ class TestSingleGlauber:
             ),
         )
 
-    def test_small_2(self, class_to_test: type[GlauberSim]):
+    def test_small_2(self, class_to_test: type[GlauberSim], tmpdir):
+        tmpdir = str(tmpdir)
         np.random.seed(0)
-        sim = class_to_test(n_interior=84, padding=3, p=0.7, t=2000, tol=0.85)
+        sim = class_to_test(n_interior=84, padding=3, p=0.7, t=2000, tol=0.85,
+                            results_dir=tmpdir)
         result = sim.run_single_glauber(False)
         assert(result["fixation"] == False and result["iterations"] == 2000)
 
-    def test_small_checkpoints(self, class_to_test: type[GlauberSim]):
+    def test_small_checkpoints(self, class_to_test: type[GlauberSim], tmpdir):
         np.random.seed(0)
         if os.path.isdir("./results/test_small_long"):
             shutil.rmtree("./results/test_small_long")
@@ -245,36 +249,45 @@ class TestSingleGlauber:
 
         assert(result_2["fixation"] == False and result_2["iterations"] == 2000)
 
-    def test_seed(self, class_to_test: type[GlauberSim]):
-        sim = class_to_test(n_interior=84, padding=3, p=0.7, t=2000, tol=0.85, random_seed=69)
+    def test_seed(self, class_to_test: type[GlauberSim], tmpdir):
+        tmpdir = str(tmpdir)
+        sim = class_to_test(n_interior=84, padding=3, p=0.7, t=2000, tol=0.85, random_seed=69,
+                            results_dir=tmpdir)
         result = sim.run_single_glauber(False)
         assert(result["fixation"] == False and result["iterations"] == 2000)
 
-    def test_zero_padding(self, class_to_test: type[GlauberSim]):
-        sim = class_to_test(n_interior=84, padding=0, p=0.7, t=2000, tol=0.85, random_seed=69)
+    def test_zero_padding(self, class_to_test: type[GlauberSim], tmpdir):
+        tmpdir = str(tmpdir)
+        sim = class_to_test(n_interior=84, padding=0, p=0.7, t=2000, tol=0.85, random_seed=69,
+                            results_dir=tmpdir)
         result = sim.run_single_glauber(False)
         assert(result["fixation"] == False and result["iterations"] == 2000)
 
     @pytest.mark.slow
-    def test_large_long(self, class_to_test: type[GlauberSim]):
+    def test_large_long(self, class_to_test: type[GlauberSim], tmpdir):
+        tmpdir = str(tmpdir)
         np.random.seed(0)
-        sim = class_to_test(n_interior=1000, padding=100, p=0.98, t=200000, tol=0.982)
+        sim = class_to_test(n_interior=1000, padding=100, p=0.98, t=200000, tol=0.982,
+                            results_dir=tmpdir)
         result = sim.run_single_glauber(verbose=True)
 
         assert result["fixation"] == True
         assert pytest.approx(result["iterations"], 200) == 162854
 
     @pytest.mark.slow
-    def test_small_long(self, class_to_test: type[GlauberSim]):
+    def test_small_long(self, class_to_test: type[GlauberSim], tmpdir):
+        tmpdir = str(tmpdir)
         np.random.seed(0)
-        sim = class_to_test(n_interior=200, padding=15, p=0.7, t=200000, tol=0.9)
+        sim = class_to_test(n_interior=200, padding=15, p=0.7, t=200000, tol=0.9,
+                            tmpdir=tmpdir)
         result = sim.run_single_glauber(verbose=True)
         assert result["fixation"] == True
         assert pytest.approx(result["iterations"], 200) == 196555
 
-    def test_small_bitmap(self, class_to_test: type[GlauberSim]):
+    def test_small_bitmap(self, class_to_test: type[GlauberSim], tmpdir):
+        tmpdir = str(tmpdir)
         sim = class_to_test(n_interior=84, padding=3, p=0.95, t=2000, tol=0.96,
-                            save_bitmaps_every=500, random_seed=0)
+                            save_bitmaps_every=500, random_seed=0, tmpdir=tmpdir)
         result = sim.run_single_glauber(False)
         assert(result["fixation"] == False and result["iterations"] == 2000)
 
