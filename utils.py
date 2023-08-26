5eceed8580b88add0bd1c0b884b0671428cecd58
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-08-26 18:53:39 +0100

    helper to process entire run

---
+++ utils.py
@@ -0,0 +1,49 @@
+import plot_bitmaps as pm
+import plot_traces
+import make_gif as mg
+import os
+
+import warnings
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+def process_run(run_dir):
+    reps = [int(x.split('-')[1]) for x in os.listdir(run_dir) if x.startswith('rep-')]
+    if len(reps) == 0:
+        return
+    for i in range(0, max(reps) + 1):
+        bitmap_path = os.path.join(run_dir, f"rep-{i}", "bitmap_results")
+        if not os.path.isdir(bitmap_path):
+            continue
+        try:
+            pm.plot_all_bitmaps_in_dir(bitmap_path)
+            mg.make_gif(bitmap_path)
+        except Exception as e:
+            print(e)
+        print(f"For rep {i} done")
+    
+    try:        
+        plot_traces.one_plot_for_all(run_dir)
+    except Exception as e:
+        print(e)
+     
+
+def process_stem(stem):
+    results = os.listdir(stem)
+    for r in results:
+        if r.startswith('.'):
+            print(f"Skipping {r}")
+            continue
+        if "traces-all.png" in os.listdir(os.path.join(stem, r)):
+            print(f"Already done {r}")
+            continue
+        print(f"Starting with {r}")
+        run_dir = os.path.join(stem, r)
+        process_run(run_dir)
+        print(f"Done with {r}")
+    
+    
+    
+if __name__ == '__main__':
+    process_stem("/Users/konrad/code/school/msc-thesis/results/from-remote")
+    
+    
