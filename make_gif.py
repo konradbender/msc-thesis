874c115979b34f8234208bb6639fe0e5bcf50e72
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2023-08-26 19:46:05 +0100

    make gif

--- make_gif.py
+++ make_gif.py
@@ -8,12 +8,16 @@ import os
 
 def make_gif(plot_dir):
     contents = os.listdir(plot_dir)
-    plots = [os.path.join(plot_dir, x) for x in contents if x.endswith('.png')]
-    plots = [(x.split('.')[0].split('-')[1], x) for x in plots]
+    plots = [x for x in contents if x.endswith('.png')]
+    plots = [(int(x.split('.')[0].split('-')[1]), x) for x in plots]
     plots.sort()
-    frames = np.stack([iio.imread(x[1]) for x in plots], axis=0)
+    frames = np.stack([iio.imread(os.path.join(plot_dir, x[1])) for x in plots], axis=0)
     
     gif_path = os.path.join(os.path.dirname(plot_dir), "animation.gif")
     iio.imwrite(gif_path, frames)
     optimize(gif_path)
+    
+if __name__ == '__main__':
+    make_gif("/Users/konrad/code/school/msc-thesis/results/from-remote/0825_21-37-01/rep-3/bitmap_results")
+    
     
